c3752ca3ad1971809911db5a7f52111095f929eb
Nabeela33 <41548534+Nabeela33@users.noreply.github.com>
2025-07-25 12:54:43 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ st.markdown("""
             font-size: 16px !important;
         }
         h1 {
-            font-size: 36px !important;
+            font-size: 42px !important;
         }
         h2 {
             font-size: 30px !important;
