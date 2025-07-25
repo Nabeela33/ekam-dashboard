02548575c97bd010bb6fc65505e7ee50c47ea0c5
Nabeela33 <41548534+Nabeela33@users.noreply.github.com>
2025-07-25 12:42:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ st.set_page_config(page_title="EKAM 2025 Sports Dashboard", layout="wide")
 st.markdown("""
     <style>
         html, body, [class*="st-"] {
-            font-family: 'Times New Roman', sans-serif !important;
+            font-family: 'Verdana', sans-serif !important;
             font-size: 14px !important;
         }
         h1 {
